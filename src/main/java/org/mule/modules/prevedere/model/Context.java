696d2d7793bd71287f39a1884cc64f65611774e4
Josh Kaminsky <josh.kaminsky@gmail.com>
2016-08-05 11:51:16 -0400

    updated license headers and added minmuleversion to connector annotation

--- src/main/java/org/mule/modules/prevedere/model/Context.java
+++ src/main/java/org/mule/modules/prevedere/model/Context.java
@@ -1,3 +1,11 @@
+/**
+ *     PREVEDERE CONFIDENTIAL
+ *
+ *     2012 Prevedere Incorporated
+ *     All Rights Reserved.
+ *
+ *     NOTICE:  All information contained herein is, and remains the property of Prevedere Incorporated and its suppliers, if any.  The intellectual and technical concepts contained herein are proprietary to Prevedere Incorporated and its suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Prevedere Incorporated.
+ */
 package org.mule.modules.prevedere.model;
 
 import java.util.List;
